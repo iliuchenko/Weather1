16af0dbcfef3efedc049beb58fc362cd83c6cfbd
Viktoriia <104028317+iliuchenko@users.noreply.github.com>
2022-07-19 20:28:14 +0100

    Display the forecast data using the API

--- index.js
+++ index.js
@@ -50,7 +50,51 @@ formattedDate.innerHTML = `${date} ${month} ${year}`;
 let formattedTime = document.querySelector(".time");
 formattedTime.innerHTML = `${hours} : ${minutes}`;
 
-// Search the city
+// Search the city and weather
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return  days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index <= 6 & index !== 0) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" class="icon-forecast" />
+        <div class="weather-forecast-temp"> 
+          <span>${Math.round(forecastDay.temp.max)}</span> 
+          <a href="#" class="celsius">&#176;C</a> |
+          <a href="#" class="fahrenheit">&#176;F</a> 
+          <br />
+          <span class="forecast-temperature-min">${Math.round(forecastDay.temp.min)}</span>  
+          <a href="#" class="celsius">&#176;C</a> |
+          <a href="#" class="fahrenheit">&#176;F</a>
+        </div>
+      </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "35b76b262ed865a4a88cd579a156ed0d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function searchWeather(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("#currentDayCelsius").innerHTML = Math.round(
@@ -80,6 +124,8 @@ function searchWeather(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function search(city) {
